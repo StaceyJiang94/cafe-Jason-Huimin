f9434bc73725c6c9362b233f87f2d6e49000aca7
Huimin Jiang <hjiang7@mail.sfsu.edu>
2020-04-20 20:01:19 -0700

    Initial Commit

---
+++ cafe-Jason-Huimin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cafe-Jason-Huimin
+//
+//  Created by Huimin Jiang on 4/20/20.
+//  Copyright © 2020 Huimin Jiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
